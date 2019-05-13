a5b69da5bffde1790e576ba6a34182218e1a79f8
albert <albertpalka1995@gmail.com>
2019-05-13 21:17:40 +0200

    feature(main.component): adding content for main route

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
-
+  user;
   constructor() { }
 
   ngOnInit() {
+    this.user = {
+      weight: 80,
+      height: 182,
+      currentDiet: 'dieta niskokaloryczna',
+      purpose: 78,
+      totalDayKcal: 2000,
+      ateKcal: 1200,
+      remainKcal: 800,
+      exercise: 200,
+      meal1: 'omlet',
+      meal2: 'owsianka',
+      meal3: 'twaróg',
+      meal4: ''
+    }
   }
-
 }
