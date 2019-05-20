f7473ec158e1bce7912052ebcfe24f3734563eae
ppietralik <pietralikpawel@gmail.com>
2019-05-20 08:55:45 +0200

    Creating basic user authentication setup

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDhFtoA_t3PkzHSWnAaGifoKig7E5cYCFE",
+    authDomain: "dietmotivator-45021.firebaseapp.com",
+    databaseURL: "https://dietmotivator-45021.firebaseio.com",
+    projectId: "dietmotivator-45021",
+    storageBucket: "dietmotivator-45021.appspot.com",
+    messagingSenderId: "409289407284",
+    appId: "1:409289407284:web:50ffe6bed4a0e05e"
+  }
 };
 
 /*
