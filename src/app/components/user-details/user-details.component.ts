71a5fe35c73eec1a4d0d38d2f608b122d8927f75
albert <albertpalka1995@gmail.com>
2019-05-23 12:13:37 +0200

    adding user details

--- src/app/components/user-details/user-details.component.ts
+++ src/app/components/user-details/user-details.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserDetailsComponent implements OnInit {
 
+  userData = {
+    name: 'Edward',
+    surname: 'Ącki',
+    weight: 80,
+    height: 180,
+    age: 24,
+    gender: 'Mężczyzna',
+    currentDiet: 'odchudzająca',
+    demandKcal: 1800,
+    BMI: 25
+  }
+
+  editableData = false;
   constructor() { }
 
   ngOnInit() {
   }
 
+  editData() {
+    this.editableData = !this.editableData;
+  }
+
+  saveData() {
+    console.log('zapisz dane: ', this.userData);
+  }
 }
