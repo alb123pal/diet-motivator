a26f89224fc57d3219e17c079766cf57b0ca68df
albert <albertpalka1995@gmail.com>
2019-05-23 17:36:43 +0200

    adding api for product list

--- src/app/components/products-list/products-list.component.ts
+++ src/app/components/products-list/products-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-products-list',
@@ -26,11 +27,29 @@ export class ProductsListComponent implements OnInit {
   foundProduct: string;
   productFilter: Object;
 
-  constructor() { }
+  constructor(private _httpClient: HttpClient) { }
 
   ngOnInit() {
+    this.getAllProducts().subscribe((data) => {
+      console.log(data);
+    })
   }
 
+  getAllProducts() {
+    const prawidlowyRequest = 'https://trackapi.nutritionix.com/v2/search/item?nix_item_id=513fc9e73fe3ffd40300109f';
+    // let body =JSON.stringify({
+    //   'query':"2 eggs",
+    //   'timezone': "US/Eastern"
+    // })
+      return this._httpClient.get(`https://trackapi.nutritionix.com/v2/search/instant?query="banan"&detailed=True`,
+        { headers: new HttpHeaders({
+          'x-app-id': '727333ed',
+          'x-app-key': '23c6b36f9b39112e54797023d2e74008',
+          'x-remote-user-id': '0'
+      }),
+    observe: 'response',
+    })
+  }
   searchProduct(event) {
     this.productFilter = {};
     if (event.target.value === '') {
