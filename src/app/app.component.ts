48a0dbd26e9d168914b99d238ec74cdf2eb6fb88
albert <albertpalka1995@gmail.com>
2019-06-01 18:26:07 +0200

    removing ionic app, adding in app component authorizeService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { AuthorizeUserService } from './services/autorize-user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  title = 'diet-motivator';
+  isLogged = false;
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
+    public userService: AuthorizeUserService
   ) {
     this.initializeApp();
+  }
+
+  ngOnInit() {
+    this.userService.isAuthorize$.subscribe((isLogged) => {
+      this.isLogged = isLogged;
+    });
   }
 
   initializeApp() {
